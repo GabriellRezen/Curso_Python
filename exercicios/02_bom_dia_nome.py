27bd89dbed3f8c902365ebaa55489774476eeb57
Gabrel Rezende <gabirezende13@gmail.com>
2026-08-19 10:16:17 -0300

    Adiciona exercícios de introdução ao Python, incluindo variáveis, operações matemáticas e interações com o usuário.

---
+++ exercicios/02_bom_dia_nome.py
@@ -0,0 +1,4 @@
+# Faça um programa que de bom dia, pergutnta o nome da pessoa e responde que é um prazer conecer ela, citando o nome da pessoa
+nome = input("Bom dia! Qual seu nome? ")
+
+print("Prazer em te conhecer", nome)
